b9cc324585e442d36766ee15aae0d7075d7e2530
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-10-17 10:53:01 -0300

    Mudança na formatação dos Arquivos
    Mudança nos espaçamentos e no tamanho dos tabs

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,93 +1,86 @@
 // create map
-const map = L.map('mapid').setView([-22.8626587,-47.2146692], 15.5);
+const map = L.map("mapid").setView([-22.8626587, -47.2146692], 15.5);
 
 // create and add titleLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [48, 58],
-    iconAnchor: [29, 68]
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [48, 58],
+  iconAnchor: [29, 68],
 });
 
 let marker;
 
 //create and add marker
-map.on('click', (event) => {
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
+map.on("click", (event) => {
+  const lat = event.latlng.lat;
+  const lng = event.latlng.lng;
 
-    document.querySelector("[name=lat]").value = lat;
-    document.querySelector("[name=lng]").value = lng;
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
-    //remove icon
-    marker && map.removeLayer(marker);
+  //remove icon
+  marker && map.removeLayer(marker);
 
-    //add icon layer
-    marker = L.marker([lat, lng], { icon })
-    .addTo(map);
-
-
+  //add icon layer
+  marker = L.marker([lat, lng], { icon }).addTo(map);
 });
 
 //add photo field
-function addPhotoField(){
-    
-    //pick up photos container #images
-    const container = document.querySelector("#images");
+function addPhotoField() {
+  //pick up photos container #images
+  const container = document.querySelector("#images");
 
-    //pick up container to duplic 
-    const fieldsContainer = document.querySelectorAll(".new-upload");
+  //pick up container to duplic
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    //realize the clonage of image
-    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+  //realize the clonage of image
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
 
-    //checking if new field is clean, if yes, no add in images container
-    if(newFieldContainer.children[0].value != ""){
+  //checking if new field is clean, if yes, no add in images container
+  if (newFieldContainer.children[0].value != "") {
+    //clear the field before to add to container
+    newFieldContainer.children[0].value = "";
 
-        //clear the field before to add to container
-        newFieldContainer.children[0].value="";
-
-        //add the clonage to images container #images
-        container.appendChild(newFieldContainer);
-    }
+    //add the clonage to images container #images
+    container.appendChild(newFieldContainer);
+  }
 }
 
 //delete image field
-function deleteField(event){
-    const span = event.currentTarget;
+function deleteField(event) {
+  const span = event.currentTarget;
 
-    const fieldsContainer = document.querySelectorAll(".new-upload")
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    //checking if there is more than one field container
-    if(fieldsContainer.length > 1){
-        
-        //delete the parent field
-        span.parentNode.remove();
-
-    }else{
-
-        //clear the image field
-        span.parentNode.children[0].value="";
-    }
+  //checking if there is more than one field container
+  if (fieldsContainer.length > 1) {
+    //delete the parent field
+    span.parentNode.remove();
+  } else {
+    //clear the image field
+    span.parentNode.children[0].value = "";
+  }
 }
 
 //select yes or no
-function toggleSelect(event){
+function toggleSelect(event) {
+  //remove the .active button class
+  document.querySelectorAll(".button-select button").forEach(
+    (button) => button.classList.remove("active")
+  );
 
-    //remove the .active button class 
-    document.querySelectorAll(".button-select button")
-    .forEach( button => button.classList.remove('active') );
+  //add the class .active in the clicked button
+  const button = event.currentTarget;
+  button.classList.add("active");
 
-    //add the class .active in the clicked button
-    const button = event.currentTarget
-    button.classList.add("active");
+  //get the clicked button
+  const input = document.querySelector("[name='open_on_weekends']");
 
-    //get the clicked button 
-    const input = document.querySelector("[name='open_on_weekends']");
-
-    //update the value input hidden
-    input.value = button.dataset.value;
-
+  //update the value input hidden
+  input.value = button.dataset.value;
 }
--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,46 +1,45 @@
 // create map
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
 };
 
-const map = L.map('mapid', options).setView([-22.8626587,-47.2146692], 15.5);
+const map = L.map("mapid", options).setView([-22.8626587, -47.2146692], 15.5);
 
 // create and add titleLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
 });
 
-
 // create and add marker
-L.marker([-22.8626587,-47.2146692], { icon }).addTo(map)
+L.marker([-22.8626587, -47.2146692], { icon }).addTo(map);
 
-function selectImage(event){
-    const button = event.currentTarget;
+function selectImage(event) {
+  const button = event.currentTarget;
 
-    //remover todas as classes .active
-    const buttons = document.querySelectorAll(".images button");
-    
-    buttons.forEach((button) => {
-        button.classList.remove("active")
-    });
+  //remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button");
 
-    //selecionar a imagem clicada
-    const image = button.children[0];
-    const imageContainer = document.querySelector(".orphanage-details > img");
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
 
-    //atualizar o container de image
-    imageContainer.src = image.src;
+  //selecionar a imagem clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
 
-    //adicionar a classe .active para este botao
-    button.classList.add("active");
+  //atualizar o container de image
+  imageContainer.src = image.src;
+
+  //adicionar a classe .active para este botao
+  button.classList.add("active");
 }
